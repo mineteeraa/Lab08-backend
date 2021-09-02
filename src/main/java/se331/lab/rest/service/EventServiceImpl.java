2fefb5bc29cb5527f41ca0563c160539c0279813
mineteeraa <patteera_mine@hotmail.com>
2021-09-02 21:35:28 +0700

    Use the Pageable Object in the Spring boot

--- src/main/java/se331/lab/rest/service/EventServiceImpl.java
+++ src/main/java/se331/lab/rest/service/EventServiceImpl.java
@@ -1,11 +1,10 @@
 package se331.lab.rest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import se331.lab.rest.dao.EventDao;
 import se331.lab.rest.entity.Event;
-
-import java.util.List;
 
 @Service
 public class EventServiceImpl implements EventService{
@@ -17,7 +16,7 @@ public class EventServiceImpl implements EventService{
     }
 
     @Override
-    public List<Event> getEvents(Integer pageSize, Integer page) {
+    public Page<Event> getEvents(Integer pageSize, Integer page) {
         return eventDao.getEvents(pageSize, page);
     }
 
